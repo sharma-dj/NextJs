ed74c9e44b420d8ee59060d306ff38fc472d3ca7
DJ <dj4388@gmail.com>
2023-07-18 00:02:03 +0530

    added new entity user

--- nextjs-remult-todo/src/pages/api/[...remult].ts
+++ nextjs-remult-todo/src/pages/api/[...remult].ts
@@ -2,7 +2,8 @@
 
 import { remultNext } from "remult/remult-next"
 import { Task } from "../../shared/Task"
+import { User } from "../../shared/User"
 
 export default remultNext({
-  entities: [Task]
+  entities: [Task,User]
 })
