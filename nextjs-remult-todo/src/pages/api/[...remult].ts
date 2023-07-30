802113bfc741f73e6da4e0a8660345caea4894a8
DJ <dj4388@gmail.com>
2023-07-30 23:54:10 +0530

    changes

--- nextjs-remult-todo/src/pages/api/[...remult].ts
+++ nextjs-remult-todo/src/pages/api/[...remult].ts
@@ -2,8 +2,8 @@
 
 import { remultNext } from "remult/remult-next"
 import { Task } from "../../shared/Task"
-import { User } from "../../shared/User"
+//import { User } from "../../shared/User"
 
 export default remultNext({
-  entities: [Task,User]
+  entities: [Task]
 })
