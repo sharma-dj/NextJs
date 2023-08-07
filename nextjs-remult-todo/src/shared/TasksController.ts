d1719ceeef41c94a41da100ef10772830b56c31e
DJ <dj4388@gmail.com>
2023-08-07 23:42:37 +0530

    added task controller 2

--- nextjs-remult-todo/src/shared/TasksController.ts
+++ nextjs-remult-todo/src/shared/TasksController.ts
@@ -4,5 +4,8 @@ import { Task } from "./Task";
 export class TaskController {
     static async setAllCompleted(completed: boolean) {
         const taskRepo = remult.repo(Task);
+        for (const task of await taskRepo.find()) {
+            await taskRepo.save({ ...task, completed });
+        }
     }
 }
